331b73fd8566962bb80c6587bb196c470226377b
robajillo <rjillo@yahoo.com>
2020-10-28 21:50:28 +0300

    created Description classes

--- Neighbor/views.py
+++ Neighbor/views.py
@@ -10,8 +10,8 @@ from .permissions import IsAdminOrReadOnly
 
 class NeighborhoodList(APIView):
     def get(self, request, format=None):
-        all_hood = Neighborhood.objects.all()
-        serializers = NeighborhoodSerializer(all_hood, many=True)
+        all_neighborhood = Neighborhood.objects.all()
+        serializers = NeighborhoodSerializer(all_neighborhood, many=True)
         return Response(serializers.data)
 
     def post(self, request, format=None):
@@ -21,6 +21,20 @@ class NeighborhoodList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class NeighborhoodDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_neighborhood(self, pk):
+        try:
+            return Neighborhood.objects.get(pk=pk)
+        except Neighborhood.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        neighborhood = self.get_hood(pk)
+        serializers = NeighborhoodSerializer(neighborhood)
+        return Response(serializers.data)
+
 
 class ProfileList(APIView):
     def get(self, request, format=None):
@@ -36,6 +50,20 @@ class ProfileList(APIView):
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ProfileDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile)
+        return Response(serializers.data)
+
+
 class UserList(APIView):
     def get(self, request, format=None):
         all_user = User.objects.all()
@@ -50,6 +78,20 @@ class UserList(APIView):
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UserDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_merch(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializers = UserSerializer(user)
+        return Response(serializers.data)
+
+
 class BusinessList(APIView):
     def get(self, request, format=None):
         all_business = Business.objects.all()
@@ -63,3 +105,16 @@ class BusinessList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BusinessDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_merch(self, pk):
+        try:
+            return Business.objects.get(pk=pk)
+        except Business.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        business = self.get_business(pk)
+        serializers = BusinessSerializer(business)
+        return Response(serializers.data)
