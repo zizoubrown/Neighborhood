8e0a0b80f5d8d3165ee1a3f925250fc25dc63d51
zizoubrown <zizumoha2015@gmail.com>
2020-10-29 02:11:13 +0300

    add imports

--- Neighbor/urls.py
+++ Neighbor/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.views import APIView
+from .serializers import *
+from .models import *
 
 urlpatterns = [
     path('api/post/auth/signup', views.MerchList.as_view())
