532d44c37019ce876dfe3632d6f14a0b741b1c32
robajillo <rjillo@yahoo.com>
2020-10-28 16:15:05 +0300

    created class ProfileSerializer~

--- Neighbor/serializers.py
+++ Neighbor/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import *
+
+class NeighborhoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neighborhood
+        fields = ('neighborhood_name', 'neighborhood_location', 'occupants_count','admin')
+
