2759c3003e2a485da6b2f39d2f59a2a01d27279a
robajillo <rjillo@yahoo.com>
2020-10-28 17:05:42 +0300

    imported status from rest_framework

--- Neighbor/urls.py
+++ Neighbor/urls.py
@@ -4,3 +4,8 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    path('api/post/auth/signup', views.MerchList.as_view())
+]
--- Neighbor/views.py
+++ Neighbor/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
 from .serializer import *
+from rest_framework import status
+
 
 
 class NeighborhoodList(APIView):
