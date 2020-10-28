c1993283e75dfc48ee9a1a7f597d7766865670ab
robajillo <rjillo@yahoo.com>
2020-10-28 16:29:48 +0300

    created urls.py and imported required files

---
+++ Neighbor/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
