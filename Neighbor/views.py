53c411e78687de614912b3e882ecf5bd40593969
robajillo <rjillo@yahoo.com>
2020-10-28 16:22:24 +0300

    created class Neighborhoodlist and defined get view function

--- Neighbor/views.py
+++ Neighbor/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializer import *
 
-# Create your views here.
+
+class NeighborhoodList(APIView):
+    def get(self, request, format=None):
+        all_hood = Neighborhood.objects.all()
+        serializers = NeighborhoodSerializer(all_hood, many=True)
+        return Response(serializers.data)
