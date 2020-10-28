5b5705d9fa1e4c06c8b2c11bbbd7f85c7d65966b
robajillo <rjillo@yahoo.com>
2020-10-28 17:09:56 +0300

    defined post view function for class User

--- Neighbor/views.py
+++ Neighbor/views.py
@@ -39,6 +39,13 @@ class UserList(APIView):
         serializers = UserSerializer(all_user, many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class BusinessList(APIView):
     def get(self, request, format=None):
         all_business = Business.objects.all()
