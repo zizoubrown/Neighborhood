dc8b4fa467b2d62e030faf725f67d287411c1dff
robajillo <rjillo@yahoo.com>
2020-10-28 17:07:30 +0300

    defined post view function for class Neighborhood

--- Neighbor/views.py
+++ Neighbor/views.py
@@ -13,6 +13,13 @@ class NeighborhoodList(APIView):
         serializers = NeighborhoodSerializer(all_hood, many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers = NeighborhoodSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class ProfileList(APIView):
     def get(self, request, format=None):
         all_profile = Profile.objects.all()
