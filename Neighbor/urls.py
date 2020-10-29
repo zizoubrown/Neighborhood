68b3e126acd2b4d13fa883815dddc0e5422200d2
zizoubrown <zizumoha2015@gmail.com>
2020-10-29 15:25:12 +0300

    modify and add path

--- Neighbor/urls.py
+++ Neighbor/urls.py
@@ -4,9 +4,14 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/post/auth/signup', views.MerchList.as_view()),
-     path('api/neighborhood/', views.NeighborhoodList.as_view()),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('api/neighborhood/', views.NeighborhoodList.as_view()),
+    path('api/neighborhood/description', views.NeighborhoodDescription.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
+    path('api/profile/description', views.ProfileDescription.as_view()),
+    
 ]
--- Neighborhood/settings.py
+++ Neighborhood/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'Neighborhood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'Neighbor/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
